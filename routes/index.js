c228f86c8d20fe114611942f230f895a0d8dbca0
tigi <onlytg@naver.com>
2018-04-25 10:18:57 +0900

    Update 'GET' method
    it can be shown api list by a path url

--- routes/index.js
+++ routes/index.js
@@ -1,75 +1,27 @@
 var express = require('express');
 var router  = express.Router();
-var fs      = require('fs');
-var path    = require('path');
-var extJson = '.json';
+var ff      = require('../routes/findFile');
 
 /* GET json path list */
-router.get('/' + extJson, function(req, res, next) {
-  var files = findFiles('./jsonFile');
+router.get('/' + ff.extJson, function(req, res, next) {
+  var files = ff.findFiles('./jsonFile');
 
-  res.json(hierarchyFiles(files));
+  res.json(ff.hierarchyFiles(files));
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var contentType = req.headers['content-type'];
-  var files = findFiles('./jsonFile');
+  var files = ff.findFiles('./jsonFile');
 
   if (contentType == 'application/json') {
-    res.json(hierarchyFiles(files));
+    res.json(ff.hierarchyFiles(files));
   } else {
     res.render('index', {
       title       : 'JSON API TEST SERVER',
-      hierarchyFiles   : hierarchyFiles(files)
+      hierarchyFiles   : ff.hierarchyFiles(files)
     });
   }
 });
-
-function findFiles(startPath) {
-  if (!fs.existsSync(startPath)){
-      console.log("No file dir ", startPath);
-      return;
-  }
-
-  var fileNames = [];
-  var files = fs.readdirSync(startPath);
-  for(var i = 0; i < files.length; i++){
-      var filename = path.join(startPath, files[i]);
-      var stat = fs.lstatSync(filename);
-      if (stat.isDirectory()) {
-        fileNames = fileNames.concat(findFiles(filename));
-      } else {
-        filename = filename.replace('jsonFile', '');
-        if (path.extname(filename) == extJson) {
-          fileNames.push(filename);
-        }
-      }
-  }
-  return fileNames;
-}
-
-function hierarchyFiles(files) {
-  var hierarchyFile = {};
-  for (var key in files) {
-    var fileName = files[key];
-    var topFolderName = fileName.match(/\/.*\//gi);
-
-    if (topFolderName) {
-      topFolderName = topFolderName.toString();
-      topFolderName = topFolderName.replace(/^\//, "");
-      topFolderName = topFolderName.replace(/\/$/, "");
-    } else {
-      topFolderName = "/";
-    }
-
-    if (!hierarchyFile[topFolderName]) {
-      hierarchyFile[topFolderName] = [];
-    }
-
-    hierarchyFile[topFolderName].push(fileName.replace(extJson, ''));
-  }
-  return hierarchyFile;
-}
 
 module.exports = router;
