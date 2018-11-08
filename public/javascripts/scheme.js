dda400744428b852ce78d04f9ca43c4301f6f87d
tigi <onlytg@naver.com>
2018-11-08 16:20:51 +0900

    Add delete message on scheme page

--- public/javascripts/scheme.js
+++ public/javascripts/scheme.js
@@ -10,3 +10,15 @@ function editButton(e, path, name, uri) {
   $inputName.val(name);
   $inputURI.val(uri);
 }
+
+function deleteButton(e) {
+  e.preventDefault();
+  var target = e.target;
+  var $form = $(target).parents("form");
+  var path = $form.find("input[name=path]").val();
+  var name = $form.find("input[name=name]").val();
+
+  if (confirm(path + "/" + name + " 항목을 삭제하시겠습니까?")) {
+    $(target).parents("form").submit();
+  }
+}
